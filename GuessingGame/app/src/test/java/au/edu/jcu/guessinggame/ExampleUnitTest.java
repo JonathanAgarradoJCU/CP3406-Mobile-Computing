4bada6452bebc10a4f66b5d2059dd1b7fafb075c
Jonathan Agarrado (Desktop) <jet320boy@gmail.com>
2023-11-23 11:34:41 +0800

    Add basic guessing game wireframe

--- GuessingGame/app/src/test/java/au/edu/jcu/guessinggame/ExampleUnitTest.java
+++ GuessingGame/app/src/test/java/au/edu/jcu/guessinggame/ExampleUnitTest.java
@@ -1,7 +1,6 @@
 package au.edu.jcu.guessinggame;
 
 import org.junit.Test;
-
 import static org.junit.Assert.*;
 
 /**
@@ -14,4 +13,19 @@ public class ExampleUnitTest {
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
     }
+
+    @Test
+    public void run_game() {
+        Game game = new Game();
+
+        boolean guessedCorrect = false;
+        for (int i = 1; i < 11; ++i) {
+            guessedCorrect = game.check(i);
+            if (guessedCorrect) {
+                break;
+            }
+        }
+        assertTrue(guessedCorrect);
+    }
 }
+
---
+++ GuessingGame/app/src/test/java/au/edu/jcu/guessinggame/Game.java
@@ -0,0 +1,24 @@
+package au.edu.jcu.guessinggame;
+
+import java.util.Random;
+
+public class Game {
+    private final int targetNumber;
+
+    public Game() {
+        Random random = new Random();
+        targetNumber = random.nextInt(100) + 1; // Generate a random number between 1-100, inclusive
+    }
+
+    public boolean check(int guess) {
+        if (guess == targetNumber) {
+            System.out.println("You guessed correctly!");
+            return true;
+        } else if (guess < targetNumber) {
+            System.out.println("Your guess is too low.");
+        } else {
+            System.out.println("Your guess is too high.");
+        }
+        return false;
+    }
+}
